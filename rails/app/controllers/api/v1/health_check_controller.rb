a540cc33e2b3fc4d332d83f6752c058da9a8111f
frkawa <k.f.kntn@gmail.com>
2024-01-21 13:01:28 +0900

    Rubocop導入

--- rails/app/controllers/api/v1/health_check_controller.rb
+++ rails/app/controllers/api/v1/health_check_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::HealthCheckController < ApplicationController
   def index
-    render json: { message: 'Health Check OK!!'}, status: :ok
+    render json: { message: 'Health Check OK!!' }, status: :ok
   end
 end
--- rails/spec/requests/api/v1/health_check_spec.rb
+++ rails/spec/requests/api/v1/health_check_spec.rb
@@ -1,15 +1,15 @@
-require "rails_helper"
+require 'rails_helper'
 
-RSpec.describe "Api::V1::HealthCheck", type: :request do
-  describe "GET /api/v1/health_check" do
-    subject { get "/api/v1/health_check" }
+RSpec.describe 'Api::V1::HealthCheck', type: :request do
+  describe 'GET /api/v1/health_check' do
+    subject { get '/api/v1/health_check' }
 
-    it "正常なレスポンスが返ること" do
+    it '正常なレスポンスが返ること' do
       subject
       json = JSON.parse(response.body)
 
       expect(response).to have_http_status(:ok)
-      expect(json["message"]).to eq("Health Check OK!!")
+      expect(json['message']).to eq('Health Check OK!!')
     end
   end
 end
