f499691df7972cea3dfb3bf9ab6241376b0903ca
frkawa <k.f.kntn@gmail.com>
2024-01-22 22:20:17 +0900

    サインアップAPI

--- rails/app/models/admin/user.rb
+++ rails/app/models/admin/user.rb
@@ -2,8 +2,8 @@
 
 class Admin::User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :registerable, :recoverable, :rememberable, :confirmable, :lockable,
+  # :recoverable, :rememberable, :confirmable, :lockable,
   # :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :validatable
+  devise :database_authenticatable, :registerable, :validatable
   include DeviseTokenAuth::Concerns::User
 end
