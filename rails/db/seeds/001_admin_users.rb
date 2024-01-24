6dea91f5022d0e5506f6859e2aa718a4f77222b6
frkawa <k.f.kntn@gmail.com>
2024-01-24 23:53:03 +0900

    fix rubocop error

--- rails/db/seeds/001_admin_users.rb
+++ rails/db/seeds/001_admin_users.rb
@@ -3,5 +3,5 @@ return if Admin::User.count.positive?
 Admin::User.create!(
   email: 'test@example.com',
   password: 'password',
-  password_confirmation: 'password'
+  password_confirmation: 'password',
 )
