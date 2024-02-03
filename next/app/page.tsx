455ff4c40751624abc1cd9bf352d3940e9b2a9e6
frkawa <k.f.kntn@gmail.com>
2024-02-03 23:28:38 +0900

    サイドバー 名前部分だけ作成

--- next/app/page.tsx
+++ next/app/page.tsx
@@ -1,4 +1,9 @@
+import Image from "next/image";
 import ArticleCard from "./(components)/ArticleCard";
+import authorIcon from "/public/author_icon.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faXTwitter } from "@fortawesome/free-brands-svg-icons";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -7,8 +12,19 @@ export default function Home() {
         <ArticleCard />
         <ArticleCard />
       </main>
-      <aside className="col-span-3 bg-white min-w-full">
-        aaa
+      <aside className="col-span-3 bg-my-middle-blue p-6 rounded-3xl mb-8 min-w-full">
+        <div className="text-center">
+          <Image src={authorIcon} alt="author icon" width={100} height={100} className="mx-auto mb-3" />
+          <div className="mb-1">AUTHOR: frkawa</div>
+          <div className="flex gap-1 justify-center">
+            <Link href="https://github.com/frkawa" rel="noopener noreferrer" target="_blank">
+              <FontAwesomeIcon icon={faGithub} className="h-4" />
+            </Link>
+            <Link href="https://x.com/frkawa_" rel="noopener noreferrer" target="_blank">
+              <FontAwesomeIcon icon={faXTwitter} className="h-4" />
+            </Link>
+          </div>
+        </div>
       </aside>
     </>
   );
