1f999da2e9f9c9d04432a7216f27980cc9cd39fb
frkawa <k.f.kntn@gmail.com>
2024-02-06 00:27:06 +0900

    日付とアイコンの高さ揃えた

--- next/app/(components)/ArticleCard.tsx
+++ next/app/(components)/ArticleCard.tsx
@@ -6,13 +6,13 @@ const ArticleCard = () => {
   return (
     <div className='bg-my-dark-blue p-6 rounded-3xl mb-8'>
       <div className='flex gap-7 text-my-emphasis-yellow'>
-        <span className='flex gap-2 font-josefinSans'>
+        <span className='flex gap-2 font-josefinSans leading-5'>
           <FontAwesomeIcon icon={faClock} className='h-4' />
-          2024/1/30 14:33
+          2024-01-30
         </span>
-        <span className='flex items-center gap-2 font-josefinSans'>
+        <span className='flex gap-2 font-josefinSans leading-5'>
           <FontAwesomeIcon icon={faArrowsRotate} className='h-4' />
-          2024/3/20 11:12
+          2024-03/20
         </span>
       </div>
       <h2 className='text-2xl font-bold my-5'>ブログ作りました</h2>
