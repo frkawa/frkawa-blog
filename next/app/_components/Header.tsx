fff7ed3430f37c6bae8b940cc5d4ded5e33f4a7d
frkawa <k.f.kntn@gmail.com>
2024-02-13 01:24:32 +0900

    fix design

--- next/app/_components/Header.tsx
+++ next/app/_components/Header.tsx
@@ -5,9 +5,9 @@ import Link from 'next/link'
 const Header = () => {
   return (
     <header className='col-span-10'>
-      <h1 className='my-12 font-josefinSans text-5xl'>
+      <h1 className='my-12 font-josefinSans text-4xl'>
         <Link href='/' className='inline-flex items-center gap-2'>
-          <FontAwesomeIcon icon={faDove} className='h-9 text-my-emphasis-sky' />
+          <FontAwesomeIcon icon={faDove} className='h-8 text-my-emphasis-sky' />
           frkawa.dev
         </Link>
       </h1>
