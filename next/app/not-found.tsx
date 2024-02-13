4381546be3729f706a17b1f7f7c60ecb91f43e21
frkawa <k.f.kntn@gmail.com>
2024-02-13 22:46:25 +0900

    not foundページ作成

---
+++ next/app/not-found.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <div className='text-center'>
+      <h1 className='mb-5 text-4xl'>404 Not Found</h1>
+      <p>
+        Oops! It seems like the page you were trying to reach isn&apos;t here.
+      </p>
+    </div>
+  )
+}
+
+export default NotFound
