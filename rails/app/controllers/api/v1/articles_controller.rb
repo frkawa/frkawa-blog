751f4471a33bc07c1e0027e7e72785e8c15a9197
frkawa <k.f.kntn@gmail.com>
2024-02-13 22:44:13 +0900

    記事取得API作成

--- rails/app/controllers/api/v1/articles_controller.rb
+++ rails/app/controllers/api/v1/articles_controller.rb
@@ -6,6 +6,15 @@ class Api::V1::ArticlesController < Api::V1::BaseController
     render json: ArticleSerializer.new(articles)
   end
 
+  def show
+    article = Article.find_by(id: params[:id])
+    if article
+      render json: ArticleSerializer.new(article)
+    else
+      render json: { error: 'Article not found.' }, status: :not_found
+    end
+  end
+
   def create
     article = current_user.articles.create!(article_params)
     render json: ArticleSerializer.new(article)
