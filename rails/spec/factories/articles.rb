2dd80e3d5066a0ed43ddbb5180e8da1de8b72265
frkawa <k.f.kntn@gmail.com>
2024-02-14 20:18:34 +0900

    API修正に伴いspec修正

--- rails/spec/factories/articles.rb
+++ rails/spec/factories/articles.rb
@@ -5,5 +5,13 @@ FactoryBot.define do
     body { Faker::Lorem.paragraphs.join("\n") }
     status { Article.status.values.sample }
     published_at { Time.current }
+
+    trait :published do
+      status { Article.status.find_value(:published) }
+    end
+
+    trait :draft do
+      status { Article.status.find_value(:draft) }
+    end
   end
 end
--- rails/spec/requests/api/v1/article_spec.rb
+++ rails/spec/requests/api/v1/article_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 RSpec.describe 'Api::V1::Article', type: :request do
   describe '記事一覧 GET /api/v1/articles' do
     before do
-      create_list(:article, 3)
+      create_list(:article, 3, :published)
+      create_list(:article, 3, :draft)
       get api_v1_articles_path
     end
 
@@ -11,8 +12,42 @@ RSpec.describe 'Api::V1::Article', type: :request do
       expect(response).to have_http_status(:ok)
     end
 
-    it '記事一覧が取得できること' do
+    it '公開済みの記事の一覧が取得できること' do
       expect(JSON.parse(response.body).length).to eq 3
+      expect(JSON.parse(response.body)).to be_all {|article| article['status'] == 'published' }
+    end
+  end
+
+  describe '記事取得 GET /api/v1/articles/[:id]' do
+    let(:published_article) { create(:article, :published) }
+    let(:draft_article) { create(:article, :draft) }
+
+    context '公開済みの記事の場合' do
+      before { get api_v1_article_path(published_article) }
+
+      it '200ステータスが返ること' do
+        expect(response).to have_http_status(:ok)
+      end
+
+      it '公開済みの記事が取得できること' do
+        expect(JSON.parse(response.body)['status']).to eq 'published'
+      end
+    end
+
+    context '下書きの記事の場合' do
+      before { get api_v1_article_path(draft_article) }
+
+      it '404ステータスが返ること' do
+        expect(response).to have_http_status(:not_found)
+      end
+    end
+
+    context '存在しない記事の場合' do
+      before { get api_v1_article_path('dummy') }
+
+      it '404ステータスが返ること' do
+        expect(response).to have_http_status(:not_found)
+      end
     end
   end
 
