2cbefeebe1d13a5729428478960a752e092e0f2c
frkawa <k.f.kntn@gmail.com>
2024-02-24 21:18:09 +0900

    新規投稿画面大枠

--- next/app/(admin)/admin/(dashboard)/articles/new/page.tsx
+++ next/app/(admin)/admin/(dashboard)/articles/new/page.tsx
@@ -1,7 +1,15 @@
 import React from 'react'
 
 const AdminArticlesNewPage = () => {
-  return <div>ArticlesNewPage</div>
+  return (
+    <div className='h-full bg-gray-200 p-5'>
+      <div className='leading-8'>
+        <h2 className='mb-6 border-b border-gray-400 pb-4 text-xl font-bold'>
+          記事新規投稿
+        </h2>
+      </div>
+    </div>
+  )
 }
 
 export default AdminArticlesNewPage
