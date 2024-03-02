7f6933a99cdffeb32d944652df9bd82e3509078e
frkawa <k.f.kntn@gmail.com>
2024-03-02 16:05:21 +0900

    format fix

--- next/app/(app)/loading.tsx
+++ next/app/(app)/loading.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Loading = () => {
   return (
-    <div className="flex justify-center" aria-label="読み込み中">
-      <div className="animate-ping h-4 mt-5 w-4 bg-my-emphasis-sky rounded-full"></div>
+    <div className='flex justify-center' aria-label='読み込み中'>
+      <div className='animate-ping h-4 mt-5 w-4 bg-my-emphasis-sky rounded-full'></div>
     </div>
   )
 }
