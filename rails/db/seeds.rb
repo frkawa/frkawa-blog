d00a2b51eef4bf5179e523d3dd488442f1f01d08
frkawa <k.f.kntn@gmail.com>
2024-03-02 13:32:39 +0900

    seed修正

--- rails/db/seeds.rb
+++ rails/db/seeds.rb
@@ -1,20 +1,13 @@
-seeds_for_every_environment
-seeds_for_development
-
-private
-
-def seeds_for_every_environment
-  seed_files = Dir.glob("#{Rails.root}/db/seeds/*.rb")
-  seed_files.each do |seed_file|
-    load(seed_file)
-  end
+# seeds_for_every_environment
+seed_files = Dir.glob("#{Rails.root}/db/seeds/*.rb")
+seed_files.each do |seed_file|
+  load(seed_file)
 end
 
-def seeds_for_development
-  return unless Rails.env.development?
+# seeds_for_development
+return unless Rails.env.development?
 
-  seed_files = Dir.glob("#{Rails.root}/db/seeds/development/*.rb")
-  seed_files.each do |seed_file|
-    load(seed_file)
-  end
+seed_files = Dir.glob("#{Rails.root}/db/seeds/development/*.rb")
+seed_files.each do |seed_file|
+  load(seed_file)
 end
--- rails/db/seeds/development/001_articles.rb
+++ rails/db/seeds/development/001_articles.rb
@@ -3,22 +3,22 @@ user = Admin::User.first
 user.articles.create!(
   [
     {
+      url: 'article-1',
       title: '公開済み記事タイトル1',
       body: '公開済み記事本文1',
       status: Article.status.published,
-      published_at: Time.current,
     },
     {
+      url: 'article-2',
       title: '下書き記事タイトル2',
       body: '下書き記事本文2',
       status: Article.status.draft,
-      published_at: Time.current,
     },
     {
+      url: 'article-3',
       title: 'アーカイブ記事タイトル3',
       body: 'アーカイブ記事本文3',
       status: Article.status.archived,
-      published_at: Time.current,
     },
   ],
 )
